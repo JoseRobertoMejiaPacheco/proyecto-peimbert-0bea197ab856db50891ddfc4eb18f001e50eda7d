6d7032ac959953cd654e28da1153fc7b9947f971
alangithub2018 <43253925+alangithub2018@users.noreply.github.com>
2022-08-27 11:05:14 -0500

    Last important commit

--- Terraform/data/init.sh
+++ Terraform/data/init.sh
@@ -124,29 +124,19 @@ echo '==========================================='
 echo '=== PASO 12: DESPLIEGUE DE CONTENEDORES ==='
 echo '==========================================='
 
-# echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# sudo apt-get update
-# sudo apt-get install -y docker.io 
-# sudo apt-get install -y docker-compose
-# sudo apt install -y git
-# cd /
-# rm -R /proyecto/
-# sudo mkdir -p /proyecto/peimbert
-# cd /proyecto/peimbert
-# git clone https://github.com/alangithub2018/proyecto-peimbert.git
-# cd /proyecto/peimbert/proyecto-peimbert/Final_Image
-# chmod 777 -R /proyecto/
-
-# docker-compose up -d
-# cd /proyecto/peimbert/proyecto-peimbert/Final_Image/MongoCluster/
-# sleep 5
-# chmod a+x rs-init.sh 
-# docker exec mongo1 /scripts/rs-init.sh
-# cd /proyecto/peimbert/proyecto-peimbert/Final_Image
-# docker-compose -f /proyecto/peimbert/proyecto-peimbert/Final_Image/docker-compose-api.yml up -d
-# chmod a+x /proyecto/peimbert/proyecto-peimbert/Final_Image/Nginx/init-letsencrypt.sh
-# ./proyecto/peimbert/proyecto-peimbert/Final_Image/Nginx/init-letsencrypt.sh
-# cd /proyecto/peimbert/proyecto-peimbert/Final_Image/Nginx
-# ./init-letsencrypt.sh
+ rm -R /proyecto
+ mkdir /proyecto
+ cd /proyecto/
+ git clone https://github.com/JoseRobertoMejiaPacheco/proyecto-peimbert-0bea197ab856db50891ddfc4eb18f001e50eda7d.git .
+ git checkout GC
+ cd MongoCluster/
+ ./dbstart.sh 
+ cd ..
+ cd ApiMongoDB/
+ docker-compose up -d
+ cd ..
+ cd Odoo15/
+ docker-compose up -d
+ cd ..
+ cd Nginx/
+ ./init-letsencrypt.sh
